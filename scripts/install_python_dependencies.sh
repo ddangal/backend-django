1c669e4fbd0d68adad3e9d4c71c2faea531199f4
Yogesh Bhandari <fireygenius@gmail.com>
2020-09-04 15:19:02 +0545

    Pipelines fixed

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -7,7 +7,7 @@ pip install -r /home/ubuntu/hackthon/requirements.txt
 #chmod a+x /home/ubuntu/env_migration.sh
 #/home/ubuntu/env_migration.sh
 cd hackthon
-gunicorn --bind 0.0.0.0:8000 hackthon.asgi 
+gunicorn --bind 0.0.0.0:8000 wsgi:application
 deactivate
 sudo cp /home/ubuntu/hackthon/gunicorn/gunicorn.service /etc/systemd/system/gunicorn.service
 sudo systemctl start gunicorn
